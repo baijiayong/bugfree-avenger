a97c648e14398bc3e5f3c616f3728663d0643fed
baijiayong <jiayongbai@gmail.com>
2014-06-03 11:31:35 +0800

    添加了空的 Order.java 的 servlet

---
+++ src/main/java/com/baldurtech/Order.java
@@ -0,0 +1,19 @@
+package com.baldurtech;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Order extends HttpServlet
+{
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+       
+    }
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+    
+    }
+}
