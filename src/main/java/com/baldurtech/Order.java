499f1e3d804a8a711d5cfba77efbe5aa4dabff64
baijiayong <jiayongbai@gmail.com>
2014-06-03 11:58:52 +0800

    ent

--- src/main/java/com/baldurtech/Order.java
+++ src/main/java/com/baldurtech/Order.java
@@ -5,12 +5,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Order extends HttpServlet
 {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-       
+       resp.getWriter().println("This is my first order_food system.");
     }
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
