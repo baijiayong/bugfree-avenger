5e5e5697fc2eba69ed18e76cd98d3aad604f48a1
baijiayong <jiayongbai@gmail.com>
2014-06-05 10:28:59 +0800

    添加了登录相关的代码

--- src/main/java/com/baldurtech/Order.java
+++ src/main/java/com/baldurtech/Order.java
@@ -2,26 +2,34 @@ package com.baldurtech;
 
 import javax.servlet.ServletException;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
 public class Order extends HttpServlet
 {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-     
-       resp.getWriter().println("This is my first order_food system.");
+        HttpSession session = req.getSession();
+        session.getAttribute("memberId");
+        resp.getWriter().println("This is my first order_food system.");
     }
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-        
         String username = req.getParameter("user_name");
         String password = req.getParameter("pass_word");
         String action = req.getParameter("action");
-             
+        
+        if("admin".equals(username) && "123".equals(password))
+        {
+            HttpSession session = req.getSession();
+            session.setAttribute("memberId",0L);
+            forward(req,resp,"loginSuccess");
+        }  
         if("register".equalsIgnoreCase(action))
         {
             resp.setCharacterEncoding("UTF-8");
