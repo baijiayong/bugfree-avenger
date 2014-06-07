395aeb5723b33074bb85c4467e2b2c45085ddfad
baijiayong <jiayongbai@gmail.com>
2014-06-07 10:48:55 +0800

    fixbug：修复忘记了设置Email的错误

--- src/main/java/com/baldurtech/Member.java
+++ src/main/java/com/baldurtech/Member.java
@@ -42,6 +42,14 @@ public class Member
     {
         return sex;
     }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
     public void setTelephone(String telephone)
     {
         this.telephone = telephone;
